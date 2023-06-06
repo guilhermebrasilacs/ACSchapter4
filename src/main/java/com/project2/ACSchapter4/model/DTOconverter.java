7c1b8ec57a7edfefc222270f48095733898eff05
Dev01 <guilhermebrasil.acs@outlook.com>
2023-06-06 09:35:12 -0300

    DTO converter

---
+++ src/main/java/com/project2/ACSchapter4/model/DTOconverter.java
@@ -0,0 +1,16 @@
+package com.project2.ACSchapter4.model;
+
+import com.project2.ACSchapter4.model.UserDTO;
+import com.project2.ACSchapter4.model.User;
+public class DTOconverter {
+    public static UserDTO convert(User user){
+        UserDTO userDTO = new UserDTO();
+        userDTO.setNome(user.getNome());
+        userDTO.setCpf(user.getCpf());
+        userDTO.setTelefone(user.getTelefone());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setEndereco(user.getEndereco());
+        userDTO.setDataCadastro(user.getDataCadastro());
+        return userDTO;
+    }
+}
